e7ece4de4476d6fe337504cb8975854c46976e51
Pawan <pathakpawan0530@gmail.com>
2025-11-27 01:11:54 +0530

    Upload Inventory Angular project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Inventory } from './pages/inventory/inventory';
+import { Customer } from './pages/customer/customer';
+import { Bill } from './pages/bill/bill';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+{path: '', redirectTo: 'home', pathMatch: 'full'},
+{path:'home',component:Home},
+{path:'Inventory',component:Inventory},
+{path:'Customer',component:Customer},
+{path:'bill',component:Bill},
+{path:'**',redirectTo:'home'},
+  
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { Inventory } from './pages/inventory/inventory';
+import { Bill } from './pages/bill/bill';
+import { Customer } from './pages/customer/customer';
+import { Home } from './pages/home/home';
+import { Header } from './pages/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink,Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('InventoryApp');
 }
